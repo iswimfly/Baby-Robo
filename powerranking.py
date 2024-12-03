e1c18b582686e7091009ed33352b2a86957373fd
iswimfly <iswimfly556@gmail.com>
2024-12-03 15:07:02 -0500

    i = 7

--- powerranking.py
+++ powerranking.py
@@ -21,7 +21,7 @@ def powerranking(game: str):
     worksheet = sheet.get_worksheet(1)
     pages: list[discord.Embed] = []
     entries = []
-    i = 1
+    i = 7
     powerrankingsheet = worksheet.get_all_values()
     while i < len(powerrankingsheet):
         if powerrankingsheet[i][1] != "":
